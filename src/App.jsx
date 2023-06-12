966d3c4067e673a35a18526b946b7e9921ace17c
Mauro <mauro10z@hotmail.com>
2023-06-12 20:41:40 -0300

    Se agrega esquema de rutas, creacion pagina principal home, creacion archivo data base, configuracion script para inicar json server desde package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './components/pages/home/Home'
 
 function App() {
 
   return (
     <>
-      <p className="read-the-docs">
-       App js
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
