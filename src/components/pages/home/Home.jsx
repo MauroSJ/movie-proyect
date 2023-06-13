b43382ddac314f32c1b4510dcfada921ef984498
Mauro <mauro10z@hotmail.com>
2023-06-13 12:45:09 -0300

    Creacion componente CardMovie

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,9 +1,31 @@
 
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import CardMovie from '../../common/cardMovie/CardMovie'
 const Home = () => {
+
+  const [movies, setMovies] =useState([])
+
+  useEffect(()=>{
+    axios.get("http://localhost:4000/movies")
+      .then((resp)=>setMovies(resp.data))
+      .catch((err)=> console.log(err))
+  }, [])
+
+
   return (
-    <div>Home</div>
+    <>
+      <div>
+        {
+          movies.map((movie)=>{
+            return(
+              <CardMovie movie={movie}  key={movie.id}/>
+              
+            )
+          })
+        }
+      </div>
+    </>
   )
 }
 
