b560571a72c6e95229c0d0fd0d5b37192bc55406
Mauro <mauro10z@hotmail.com>
2023-06-14 17:17:38 -0300

    Se agrega filtro botones y filtro de like

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -3,11 +3,13 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import CardMovie from '../../common/cardMovie/CardMovie'
 import styles from "./Home.module.css"
+import Header from '../../common/header/Header'
 
 const Home = () => {
 
   const [movies, setMovies] =useState([])
   const [dispatchLike, setDispatchLike] =useState([false])
+  const [favorite, setFavorite] =useState(false)
 
   useEffect(()=>{
     axios.get("http://localhost:4000/movies")
@@ -24,12 +26,21 @@ const Home = () => {
     .catch((err)=>console.log(err))
   }
 
+  const moviesFilter = movies.filter(movie => movie.isLiked)
+
 
   return (
     <>
+      <Header setFavorite={setFavorite}/>
       <div className={styles.containerCards}>
         {
+          !favorite ?
           movies.map((movie)=>{
+            return(
+              <CardMovie movie={movie}  key={movie.id} handleLike={handleLike}/>
+              
+            )
+          }) :  moviesFilter.map((movie)=>{
             return(
               <CardMovie movie={movie}  key={movie.id} handleLike={handleLike}/>
               
